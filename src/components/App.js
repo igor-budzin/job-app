d85289f366775d917618f750c60d159640e76971
Igor <igor.budzin95@gmail.com>
2018-04-17 17:26:06 +0300

    tmp commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,22 @@
 import React, {Component} from 'react';
 import {Row, Col} from 'antd';
+import 'antd/lib/row/style';
+import 'antd/lib/col/style';
 import SearchContainer from './search/SearchContainer';
 
 export default class App extends Component {
 
     render() {
         return (
-            <div className="app wrapper">
+            <div className="app">
 				<Row>
-					<Col span={12}>
+					<Col span={24}>
 						<SearchContainer />
 					</Col>
+				</Row>
+				<Row gutter={24}>
+					<Col span={15}></Col>
+					<Col span={9}></Col>
 				</Row>
             </div>
         );
