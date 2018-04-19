671f32a83e245776f0f5772587458da6e4208208
Igor <igor.budzin95@gmail.com>
2018-04-19 17:39:12 +0300

    tmp commit

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,57 @@ export default class App extends Component {
 					</Col>
 				</Row>
 				<Row gutter={24}>
-					<Col span={15}></Col>
+					<Col span={15}>
+						<div className="posts">
+							<div className="post">
+								<div className="title">Гід-екскурсовод для пивної екскурсії</div>
+								<div className="more">
+									<span className="money">12 000 грн.</span>
+									<span className="company">LvivBuddy</span>
+									<span className="city">Львів</span>
+								</div>
+								<div className="description">
+									Ти любиш пиво? розумієшся в його видах зокрема кравтових?
+									Можеш розказати щось про історію пивоваріння у Львові англійською чи німецькою (інша мова як +)і
+									 не маєш проблем зі спілкування цими мовами? Тоді це ідеальна робота для тебе! Надсилай резюме (англійською)
+									і можливо регулярне цікаве спілкув...
+								</div>
+								<div className="time">3 дні тому</div>
+							</div>
+
+							<div className="post">
+								<div className="title">Гід-екскурсовод для пивної екскурсії</div>
+								<div className="more">
+									<span className="money">12 000 грн.</span>
+									<span className="company">LvivBuddy</span>
+									<span className="city">Львів</span>
+								</div>
+								<div className="description">
+									Ти любиш пиво? розумієшся в його видах зокрема кравтових?
+									Можеш розказати щось про історію пивоваріння у Львові англійською чи німецькою (інша мова як +)і
+									 не маєш проблем зі спілкування цими мовами? Тоді це ідеальна робота для тебе! Надсилай резюме (англійською)
+									і можливо регулярне цікаве спілкув...
+								</div>
+								<div className="time">3 дні тому</div>
+							</div>
+
+							<div className="post">
+								<div className="title">Гід-екскурсовод для пивної екскурсії</div>
+								<div className="more">
+									<span className="money">12 000 грн.</span>
+									<span className="company">LvivBuddy</span>
+									<span className="city">Львів</span>
+								</div>
+								<div className="description">
+									Ти любиш пиво? розумієшся в його видах зокрема кравтових?
+									Можеш розказати щось про історію пивоваріння у Львові англійською чи німецькою (інша мова як +)і
+									 не маєш проблем зі спілкування цими мовами? Тоді це ідеальна робота для тебе! Надсилай резюме (англійською)
+									і можливо регулярне цікаве спілкув...
+								</div>
+								<div className="time">3 дні тому</div>
+							</div>
+						</div>
+					</Col>
 					<Col span={9}></Col>
 				</Row>
             </div>
