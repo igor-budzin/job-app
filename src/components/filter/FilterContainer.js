2267c6652187297bb3a4fc3d87b2e934b7b63cdf
Igor <igor.budzin95@gmail.com>
2018-05-04 16:16:17 +0300

    tmp

--- src/components/filter/FilterContainer.js
+++ src/components/filter/FilterContainer.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Row, Col, Checkbox} from 'antd';
+import {Row, Col, Checkbox, Input} from 'antd';
 import 'antd/lib/checkbox/style';
 
 export default class FilterContainer extends Component {
@@ -17,12 +17,23 @@ export default class FilterContainer extends Component {
 					<h6>Тип зайнятості</h6>
 					<div className="body">
 						<Checkbox.Group style={{ width: '100%' }}>
-							<Row><Checkbox value="A">Повна зайнятість</Checkbox></Row>
-							<Row><Checkbox value="B">Часткова зайнятість</Checkbox></Row>
-							<Row><Checkbox value="C">Проектна робота</Checkbox></Row>
-							<Row><Checkbox value="D">Волонтерство</Checkbox></Row>
-							<Row><Checkbox value="E">Стажування</Checkbox></Row>
+							<Row><Checkbox value="full">Повна зайнятість</Checkbox></Row>
+							<Row><Checkbox value="part">Часткова зайнятість</Checkbox></Row>
+							<Row><Checkbox value="project">Проектна робота</Checkbox></Row>
+							<Row><Checkbox value="volunteer">Волонтерство</Checkbox></Row>
+							<Row><Checkbox value="probation">Стажування</Checkbox></Row>
 						</Checkbox.Group>
+					</div>
+				</div>
+
+				<div className="filter">
+					<h6>Заробітня плата</h6>
+					<div className="body">
+						<Input.Group compact>
+							<Input style={{ width: 125, textAlign: 'center' }} placeholder="Мінімальна" />
+							<Input style={{ width: 30, borderLeft: 0, pointerEvents: 'none', backgroundColor: '#fff' }} placeholder="-" disabled />
+							<Input style={{ width: 125, textAlign: 'center', borderLeft: 0 }} placeholder="Максимальна" />
+						</Input.Group>
 					</div>
 				</div>
 			</div>
